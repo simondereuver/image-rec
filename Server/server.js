d2598625eeaacf663e553f784619d4bff57ba2cd
Simon de Reuver <dereuversimon@gmail.com>
2023-07-08 15:54:01 +0200

    Fixed simple design for all pages of website, minor error in server aswell

--- Server/server.js
+++ Server/server.js
@@ -2,21 +2,24 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-//create instance of express application
+
+//Create instance of express application
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
 
+//Give access to client-side files
 app.use(express.static(path.join(__dirname, '../Client')));
 
+//Used to fetch the topnavbar into the different pages of website
 app.get('/topnavbar.html', (req, res) => {
     const filePath = path.join(__dirname, '../Client/topnavbar.html');
     res.sendFile('C:/work/image-rec-proj/Client/topnavbar.html');
 });
 
-//start server and listen to port 1337
+//Start server and listen to port 1337
 const port = 1337;
 app.listen(port, () => {
     console.log('Server is listening on port 1337');
