6890b4d5c216c979b799b3a31c0d67a4f919576e
Simon de Reuver <dereuversimon@gmail.com>
2023-07-23 00:16:37 +0200

    Completed random quote issue

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,8 @@
 //Imports
-const express = require('express');
 const path = require('path');
 const http = require('http');
+const fs = require('fs');
+const express = require('express');
 
 //Create instance of express application
 const app = express();
@@ -17,6 +18,23 @@ app.use(express.static(path.join(__dirname, '../Client')));
 app.get('/topnavbar.html', (req, res) => {
     const filePath = path.join(__dirname, '../Client/topnavbar.html');
     res.sendFile('C:/work/image-rec-proj/Client/topnavbar.html');
+});
+
+//API endpoint for a random quote
+app.get('/api/random-quote', (req, res) => {
+    fs.readFile('./random-quotes.txt', 'utf8', (error, data) => {
+        if (error) {
+            //console.log(error);
+            res.status(500).json({ error: 'Internal Server Error'});
+            return;
+        }
+        const randomQuotesDocument = data.toString();
+        const randomQuotes = randomQuotesDocument.split('\n'); //splits the text in document into substrings and each substring is an element in randomQuotes variable
+        const randomIndex = Math.floor(Math.random() * randomQuotes.length);
+        const randomQuote = randomQuotes[randomIndex].trim();
+
+        res.json({ text: randomQuote});
+    });
 });
 
 //Start server and listen to port 1337
